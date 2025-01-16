47ed54e2282965503e9d179e93301363db1956f0
Liony Thungari <lionythungari@gmail.com>
2025-01-16 20:12:47 +0800

    update 3

--- js/script.js
+++ js/script.js
@@ -1,8 +1,35 @@
-<script>
-  function welcomeUser() {
-    const name = prompt("Masukkan nama Anda:");
-    document.getElementById("welcome-message").innerText = `Hai, ${name}! Selamat datang di situs kami.`;
+console.log("Hello world Jess");
+
+// function to validate form
+function formValidation() {
+  console.log("form validation has been clicked");
+
+  let nameInput = document.getElementById("name-input").value;
+
+  console.log(nameInput);
+  //condition for validate form
+  if (nameInput == "") {
+    alert("Name must be filled out");
+  } else {
+    //display reslut form
+    document.getElementById("result-form").innerHTML = nameInput;
   }
-  window.onload = welcomeUser;
-</script>
-<div id="welcome-message"></div>
+}
+
+document
+  .getElementById("submit-button")
+  .addEventListener("click", formValidation);
+
+changeBackground();
+// function to change background banner
+function changeBackground() {
+  let bannerList = document.getElementByClassName("banner-image");
+  console.log("bannerList");
+
+  //Looping to change background
+  for (let i = 0; i < bannerList.length; i++) {
+    console.log(i);
+    console.log(bannerList[i]);
+    bannerList[i].style.display = "none";
+  }
+}
